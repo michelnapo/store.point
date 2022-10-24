3022168a7db30d4664f1b27eb26ca2ec8e47781e
Michel Napo <michelnapo@proton.me>
2022-10-24 14:54:54 -0300

    split files and solve compilation problems

---
+++ hardhat/contracts/Store.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+
+import "./StoreTypes.sol";
+
+// To be used when getting Store contract.
+interface IStore 
+{
+}
+
+contract Store is
+    IStore,
+    Initializable, 
+    UUPSUpgradeable, 
+    OwnableUpgradeable, 
+    ReentrancyGuard 
+{
+    
+    function initialize() public initializer onlyProxy {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+        theme = Theme({
+            background:'white',
+            primary:'indigo',
+            text:'black'
+        });
+
+        //emit NewStoreEvent(address(this), storeName, block.timestamp);
+    }
+
+    using Counters for Counters.Counter;
+    Counters.Counter internal _tokenIds;
+
+    Theme theme;
+
+    // Product Arrays and Mappings
+    mapping(uint256 => Product) public tokenIdToProduct;
+
+
+    // Events
+    event NewStoreEvent(
+        address storeAddress,
+        string name, 
+        uint256 timestamp
+    );
+
+    event NewProductEvent(
+        address nft,
+        uint tokenId,
+        uint price,
+        address owner, 
+        uint256 timestamp
+    );
+
+    event ProductSoldEvent(        
+        uint itemId,
+        address nft,
+        uint tokenId,
+        uint price,
+        address from,
+        address to
+    );
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+
+    function addProductToStore(
+        string memory name,
+        uint price,
+        string memory metadata) external 
+    {
+        ///
+    }
+
+    function buyProduct(uint _tokenId) external payable {
+        ///
+        /// emit ProductSoldEvent(_tokenId, ownerAddress, msg.sender, block.timestamp);
+    }
+
+    function getProducts() external view returns(Product[] memory) 
+    {
+        ///
+    }
+
+    function getProductByTokenId(uint tokenId) external view returns(Product memory) {
+        return tokenIdToProduct[tokenId];
+    }
+
+    // Method for getting setted theme
+    function getTheme() public view returns (Theme memory) {
+        return theme;
+    }
+
+    // Method for setting store theme
+    function setTheme(string calldata _background, string calldata _primary, string calldata _text) public payable {
+        theme = Theme({
+            background: _background,
+            primary: _primary,
+            text: _text
+        });
+    }
+}
--- hardhat/contracts/StoreIndex.sol
+++ hardhat/contracts/StoreIndex.sol
@@ -9,25 +9,27 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-// Import social.point codebase
-import "social-point-contracts/contracts/PointSocial.sol";
 
-// To be used when getting Store contract.
-interface IStore 
-{
-}
+import "./StoreTypes.sol";
 
 contract StoreIndex is 
     Initializable, 
     UUPSUpgradeable, 
     OwnableUpgradeable, 
-    ReentracyGuard 
+    ReentrancyGuard 
 {
     using Counters for Counters.Counter;
     Counters.Counter internal _storeIds;
 
     // Product Arrays and Mappings
-    mapping(address => Store) public createdStores;
+    mapping(address => StoreInfo) public createdStores;
+
+    function initialize() public initializer onlyProxy {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
 
     // Store Functions
     // Example: "My Store", "A quality store", "store.jpg"
@@ -43,123 +45,12 @@ contract StoreIndex is
         // emit NewStoreEvent(newStoreId, name, block.timestamp);
     }
 
-    function getStores() external view returns(Store[] memory) {
+    function getStores() external view returns(StoreInfo[] memory) {
         //
     }
 
-     function getStoreByAddress(address storeAddress) external view returns(Store[] memory) {
-        return stores[];
-    }
+    //function getStoreByAddress(address storeAddress) external view returns(Store[] memory) {
+    //    return stores[];
+    //}
 }
 
-contract Store is
-    IStore,
-    Initializable, 
-    UUPSUpgradeable, 
-    OwnableUpgradeable, 
-    ReentracyGuard 
-{
-    constructor(string storeName)
-    {
-        theme = Theme({
-            background:'white',
-            primary:'indigo',
-            text:'black'
-        });
-
-        emit NewStoreEvent(address(this), name, block.timestamp);
-    }
-
-    using Counters for Counters.Counter;
-    Counters.Counter internal _tokenIds;
-
-    struct Theme{
-        string background;
-        string primary;
-        string text;
-    }
-
-    struct UserInfo {
-        address walletAddress;
-        string dataStorageHash;
-    }    
-
-    struct Store {
-        string name;
-        string description;
-        string logo;
-    }
-
-    struct Product {
-        IERC721 nft;
-        uint tokenId;
-        uint price;
-        address owner;
-        bool sold;
-    }
-
-    Theme theme;
-
-    // Product Arrays and Mappings
-    mapping(uint256 => Product) public tokenIdToProduct;
-
-    // Events
-    event NewStoreEvent(
-        address storeAddress,
-        string name, 
-        uint256 timestamp
-    );
-
-    event NewProductEvent(
-        address nft,
-        uint tokenId,
-        uint price,
-        address owner, 
-        uint256 timestamp
-    );
-
-    event ProductSoldEvent(        
-        uint itemId,
-        address nft,
-        uint tokenId,
-        uint price,
-        address from,
-        address to
-    );
-
-    function addProductToStore(
-        string memory name,
-        uint price,
-        string memory metadata) external 
-    {
-        ///
-    }
-
-    function buyProduct(uint _tokenId) external payable {
-        ///
-        /// emit ProductSoldEvent(_tokenId, ownerAddress, msg.sender, block.timestamp);
-    }
-
-    function getProducts() external view returns(Product[] memory) 
-    {
-        ///
-    }
-
-    function getProductByTokenId(uint tokenId) external view returns(Product memory) {
-        return tokenIdToProduct[tokenId];
-    }
-
-    // Method for getting setted theme
-    function getTheme() public view returns (Theme memory) {
-        return theme;
-    }
-
-    // Method for setting store theme
-    function setTheme(string calldata _background, string calldata _primary, string calldata _text) public payable {
-        theme = Theme({
-            background: _background,
-            primary: _primary,
-            text: _text
-        });
-    }
-}
