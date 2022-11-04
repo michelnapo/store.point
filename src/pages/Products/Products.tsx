ec2ee2985828267bdbf60fb3c44f6c705c68bfdf
Michel Napo <michelnapo@proton.me>
2022-11-03 23:21:35 -0300

    Add button to Add product page

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,18 +1,32 @@
-import { MainTitle, SettingsHeader } from "../../components";
+import { useNavigate } from "react-router-dom";
+import { RoutesEnum } from "../../@types/enums";
+import { MainTitle, PrimaryButton, SettingsHeader } from "../../components";
 import { ProductList } from "../../components/Product/ProductList";
-
 import PageLayout from "../../layouts/PageLayout";
 
-const Products = () => (
-  <PageLayout>
-    <SettingsHeader />
-    <main className="mt-12 mx-auto" style={{ maxWidth: "1000px" }}>
-      <MainTitle>Trending Index</MainTitle>
-      <section className="mb-8 mt-10">
-        <ProductList />
-      </section>
-    </main>
-  </PageLayout>
-);
+const Products = () => {
+  const navigate = useNavigate();
+
+  return (
+    <PageLayout>
+      <SettingsHeader />
+      <main className="mt-12 mx-auto" style={{ maxWidth: "1000px" }}>
+        <MainTitle>
+          <div className="flex">
+            <div className="flex-1">Trending Index</div>
+            <PrimaryButton
+              onClick={() => navigate(RoutesEnum.add_product)}
+            >
+              Add product
+            </PrimaryButton>
+          </div>
+        </MainTitle>
+        <section className="mb-8 mt-10">
+          <ProductList />
+        </section>
+      </main>
+    </PageLayout>
+  )
+};
 
 export default Products;
